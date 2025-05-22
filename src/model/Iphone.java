e63bcdfb216269e52b427bf4f1fcb700bb27194e
AnaBeatrizBentoSilva <anabento3807@gmail.com>
2025-05-22 15:27:33 -0300

    Projeto do Iphone

---
+++ src/model/Iphone.java
@@ -0,0 +1,40 @@
+package model;
+
+import interfaces.AparelhoTelefonico;
+import interfaces.NavegadorInternet;
+import interfaces.ReprodutorMusical;
+
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet{
+    //Método do reprodutor musical
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    //Método do aparelho telefônico
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + ".");
+    }
+    public void atender() {
+        System.out.println("Atendendo ligação.");
+    }
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    //Método do navegador de internet
+    public void exibirPagina(String url){
+        System.out.println("Exibindo página: " + url);
+    }
+    public void adicionarNovaAba(){
+        System.out.println("Nova aba adicionada.");
+    }
+    public void atualizarPagina(){
+        System.out.println("Página atualizada.");
+    }
+}
